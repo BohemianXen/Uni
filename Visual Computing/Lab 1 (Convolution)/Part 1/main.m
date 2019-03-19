05766c52f684db442a319d36adf9d66e1c75e704
BohemianXen <34287841+BohemianXen@users.noreply.github.com>
2019-03-19 23:26:50 +0000

    Removing visual computing files

--- Visual Computing/Lab 1 (Convolution)/Part 1/main.m
+++
@@ -1,7 +0,0 @@
-% basic convolution 
-close all
-image = im2double(imread('cameraman.tif'));
-kernel = ones(5) / 25; %[-1 0 1];
-filtered = basic_convolution(image, kernel);
-subplot(121); imshow(image); title('Input image');
-subplot(122); imshow(filtered); title('Filtered image');
