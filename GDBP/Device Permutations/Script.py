645b8c7b85a59ef73e41c436a3ef7fcd353c82af
BohemianXen <34287841+BohemianXen@users.noreply.github.com>
2019-03-30 00:34:21 +0000

    Updated logic so script is suited for dynamic changes to the input (i.e. no code change needed to add more component types)

--- GDBP/Device Permutations/Script.py
+++ GDBP/Device Permutations/Script.py
@@ -1,9 +1,8 @@
 import Components
 import csv
 from datetime import datetime
+from itertools import product
 
-# TODO: - Try/catch read/writes
-#       - Multiple sensor logic (for now different string IDs is the only way)
 
 # Expected input file indexes
 index = {
@@ -20,15 +19,6 @@ components_filename = 'components.csv'
 permutations_filename = 'permutations'
 
 
-# Fixed Unit Costs (5000 batch)
-enclosure_cost = 0
-pcb_cost = 0
-software_cost = 0
-battery_cost = 0
-misc = 0.0
-total_fixed_costs = enclosure_cost + pcb_cost + software_cost + battery_cost + misc
-
-
 # Constraints
 constraints = {
     'cost': 125.00,  # GBP
@@ -37,7 +27,6 @@ constraints = {
 
 
 # Possible Permutations
-variants = []
 viable = []
 non_viable = []
 
@@ -45,109 +34,64 @@ non_viable = []
 # Read components
 def read_components(filename):
     with open(filename, 'r') as f_in:
-
-        MEM = []
-        CON = []
-        MOT = []
-        PRE = []
-        HRT = []
-        AIR = []
-        PCB = []
-        BLE = []
-        WIFI = []
-        WANT = []
-        GPS = []
-        BAT = []
-        ENC = []
+        id_headers = []
+        components = {}
 
         for line in f_in:
-            if line is not '\n':
+            if line is not '\n' and line.find('Component Name') == -1:
                 fields = line.split(',')
-                if 'MEM' in fields[index['ID']]:
-                    MEM.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'CON' in fields[index['ID']]:
-                    CON.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'MOT' in fields[index['ID']]:
-                    MOT.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'PRE' in fields[index['ID']]:
-                    PRE.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'HRT' in fields[index['ID']]:
-                    HRT.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'AIR' in fields[index['ID']]:
-                    AIR.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'PCB' in fields[index['ID']]:
-                    PCB.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'BLE' in fields[index['ID']]:
-                    BLE.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'WIFI' in fields[index['ID']]:
-                    WIFI.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'WANT' in fields[index['ID']]:
-                    WANT.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'GPS' in fields[index['ID']]:
-                    GPS.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'BAT' in fields[index['ID']]:
-                    BAT.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'ENC' in fields[index['ID']]:
-                    ENC.append(
-                        Components.Component(fields[index['ID']], fields[index['cost']], fields[index['volume']], fields[index['quantity']]))
-                elif 'Name' in fields[index['name']]:
-                    continue
-                else:
-                    print('Error determining component')
-                    return False
+                full_id = fields[index['ID']]
+                header = full_id[:full_id.find('_')]
 
-    okay = create_permutations(MEM, CON, MOT, PRE, HRT, AIR, PCB, BLE, WIFI, WANT, GPS, BAT, ENC)
+                if header not in id_headers:
+                    id_headers.append(header)
+                    components[header] = []
+
+                components[header].append(Components.Component(fields[index['ID']],
+                                                               fields[index['cost']],
+                                                               fields[index['volume']],
+                                                               fields[index['quantity']]))
+
+    okay = create_permutations(components)
     return okay
 
 
-# Create permutations (remember to account for quantities
-def create_permutations(MEM, CON, MOT, PRE, HRT, AIR, PCB, BLE, WIFI, WANT, GPS, BAT, ENC):
+# Create permutations
+def create_permutations(components):
+    component_groupings = []
+
+    # Split all components into their respective groupings by ID
+    for header, component in components.items():
+        component_groupings.append(component)
+
+    # Cartesian product (nested for loop) to determine all possible permutations
+    component_groupings = tuple(component_groupings)
+    all_permutations = list(product(*component_groupings))
+
+    digits = len(str(len(all_permutations)))  # Max number of digits needed to uniquely identify all permutations
     ID = 0
-    # TODO: Loop will now be different since only one class needed for components
-    for enc in ENC:
-        for bat in BAT:
-            for gps in GPS:
-                for want in WANT:
-                    for wifi in WIFI:
-                        for ble in BLE:
-                            for pcb in PCB:
-                                for air in AIR:
-                                    for hrt in HRT:
-                                        for pre in PRE:
-                                            for mot in MOT:
-                                                for con in CON:
-                                                    for mem in MEM:
-                                                        comps = [mem, con, mot, pre, hrt, air, pcb, ble, wifi, want, gps, bat, enc]
-                                                        ids = list(map(lambda comp: comp.ID, comps))
-                                                        [total_cost, total_volume] = sum_components(comps)
-                                                        total_cost += total_fixed_costs
 
-                                                        current_permutation = Components.Permutations('PER_' + format(ID, '05d'),
-                                                                                                      ids,
-                                                                                                      total_cost,
-                                                                                                      total_volume)
+    for permutation in list(all_permutations):
+        ids = [comp.ID for comp in permutation]  # Extract ids of each component in the permutation
+        [total_cost, total_volume] = sum_components(permutation)  # Add all costs and volumes of components in set
 
-                                                        if (current_permutation.cost <= constraints['cost']): # and (current_permutation.volume <= constraints['volume']):
-                                                            viable.append(current_permutation)
-                                                        else:
-                                                            non_viable.append(current_permutation)
+        # Log details of current permutation (constituents, total cost, and total volume)
+        current_permutation = Components.Permutations('PER_' + format(ID, '0' + str(digits) + 'd'),
+                                                      ids,
+                                                      total_cost,
+                                                      total_volume)
 
-                                                        ID += 1
+        # Check constraints to determine permutation viability
+        if current_permutation.cost <= constraints['cost']: # and (current_permutation.volume <= constraints['volume']):
+            viable.append(current_permutation)
+        else:
+            non_viable.append(current_permutation)
 
-    okay = record_permutations()
-    return okay
+        ID += 1
+
+    # Write permutations to file (viable first)
+    record_permutations(permutations_viable=True, final=False)
+    return True
 
 
 def sum_components(components):
@@ -161,35 +105,27 @@ def sum_components(components):
 
 
 # Write permutations to file
-def record_permutations():
-    # Viable Permutations
-    with open('viable ' + permutations_filename + ' ' + datetime.now().strftime("%Y-%m-%d %H_%M_%S") + '.csv',
+def record_permutations(permutations_viable, final):
+    [viability, permutations] = ['viable', viable] if permutations_viable else ['non-viable', non_viable]
+
+    # Write permutations
+    with open(viability + ' ' + permutations_filename + ' ' + datetime.now().strftime("%Y-%m-%d %H_%M_%S") + '.csv',
               'w',  newline='') as f:
         f_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         f_writer.writerow(['ID', 'Total Cost (£)', 'Total Volume (mm3)', 'Components'])
 
-        for permutation in viable:
-            components = ''
-            for component in permutation.components:
-                components += component + '; '
-            f_writer.writerow([permutation.ID, str(permutation.cost), str(permutation.volume), components])
+        if len(permutations) != 0:
+            for permutation in permutations:
+                components = ''
+                for component in permutation.components:
+                    components += component + '; '
+                f_writer.writerow([permutation.ID, str(permutation.cost), str(permutation.volume), components])
 
-    # Non-Viable Permutations
-    with open('non-viable ' + permutations_filename + ' ' + datetime.now().strftime("%Y-%m-%d %H_%M_%S") + '.csv',
-              'w',  newline='') as f:
-        f_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        f_writer.writerow(['ID', 'Total Cost (£)', 'Total Volume (mm3)', 'Components'])
-
-        for permutation in non_viable:
-            components = ''
-            for component in permutation.components:
-                components += component + '; '
-            f_writer.writerow([permutation.ID, str(permutation.cost), str(permutation.volume), components])
-
-    return True
+    # Write non-viable permutations
+    if not final:
+        record_permutations(permutations_viable=False, final=True)
 
 
 if __name__ == '__main__':
     success = read_components(components_filename)
-    print('Fixed Costs: ' + str(total_fixed_costs))
     print('Script Completed: ' + str(success))
