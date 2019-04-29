ed2cc30b73ad6374f5f3d85fc3bb3ae656a0806d
BohemianXen <34287841+BohemianXen@users.noreply.github.com>
2019-04-30 00:03:37 +0100

    - updated template HistoryView

--- GDBP/GAIT_UI/views/history_view.py
+++ GDBP/GAIT_UI/views/history_view.py
@@ -2,11 +2,14 @@ from PyQt5.QtWidgets import QWidget
 import pyqtgraph as pg
 from views.ui_files.history_view_ui import Ui_HistoryView
 from application.Logger import Logger
-
+import numpy as np
 
 class HistoryView(QWidget):
     def __init__(self, controller):
         super().__init__()
+
+        pg.setConfigOption('background', (29, 29, 49))
+        pg.setConfigOption('foreground', 'w')
 
         self._controller = controller
         self._ui = Ui_HistoryView()
@@ -16,6 +19,9 @@ class HistoryView(QWidget):
         self._logger = Logger(self.name)
 
         self._ui.expandPushButton.clicked.connect(lambda: self._controller.expand_button_clicked())
+        x = np.random.normal(size=1000)
+        y = np.random.normal(size=1000)
+        self._ui.graphicsView.plot(x, y, pen=None, symbol='o')
 
     def toggle_calendar_view(self):
         if self._ui.calendarWidget.isVisible():
