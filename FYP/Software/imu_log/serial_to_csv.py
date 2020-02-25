a7082cb64b1b63a5b85333f10e9d617290dbb080
BohemianXen <34287841+BohemianXen@users.noreply.github.com>
2020-02-25 04:13:18 +0000

    - Now uses locally captured ~2s capture (238 samples) - Capture triggered with button on D5 which needs debouncing (TODO) - Python csv script now not a bottleneck

--- FYP/Software/imu_log/serial_to_csv.py
+++ FYP/Software/imu_log/serial_to_csv.py
@@ -4,7 +4,7 @@ import csv
 
 
 class SerialToCSV:
-    def __init__(self, port_no=5, rate=115200, timeout=1,  samples=10):
+    def __init__(self, port_no=5, rate=115200, timeout=1,  samples=238):
         self.samples = samples
         self.data = []
         self.port = serial.Serial('COM{}'.format(port_no), rate, timeout=timeout)
@@ -15,21 +15,22 @@ class SerialToCSV:
             self.port.open()
 
     def stream(self):
-        self.streaming = True
+        streaming = True
         with open('test.csv', 'w', newline='') as f:
             col_headers = ['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'mx', 'my', 'mz']
             f_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             f_writer.writerow(col_headers)
 
-            while self.streaming and self.samples is not 0:
+            while streaming and self.samples is not 0:
                 try:
                     line = self.port.readline()
                     str_line = line.decode('utf-8').rstrip(',\r\n')
-                    self.data.append(str_line)
-                    print(str_line)
-                    self.samples -= 1
+                    if str_line != '':
+                        self.data.append([(float(x))/1000.0 for x in str_line.split(',')])
+                        print(self.data[-1])
+                        self.samples -= 1
                 except Exception as e:
-                    self.streaming = False
+                    streaming = False
                     print(e)
 
     def write_data(self):
@@ -38,7 +39,7 @@ class SerialToCSV:
             f_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             f_writer.writerow(col_headers)
             for line in self.data:
-                f_writer.writerow(line.split(','))
+                f_writer.writerow(line)
 
 
 if __name__ == '__main__':
@@ -48,6 +49,6 @@ if __name__ == '__main__':
         'timeout': 1,
         'samples': 10
     }
-    serial_to_csv = SerialToCSV(params['port_no'] , params['rate'], params['timeout'])
+    serial_to_csv = SerialToCSV(params['port_no'], params['rate'], params['timeout'])
     serial_to_csv.stream()
     serial_to_csv.write_data()
