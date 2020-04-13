- Added better ReLU activated SMV model - Fixed standard deviation accumulation, was missing a simple re-initialisation - Better LED signalling - Red LED (Pin 4) needs re-soldering as connection is temperamental

FYP/Software/Training/gui.py
@@ -19,7 +19,7 @@ tf.get_logger().setLevel('DEBUG')  # Reduce logging
 
 params = {
     'live mode': True,
-    'quick capture': False,  # TODO: Ensure standard capture mode isn't broken
+    'quick capture': False,
     'on-board predict': True,  # TODO: Auto-predict mode from advertised service
     'play audio': False,
     'name': 'FallDetector',